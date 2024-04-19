f62a48dc1ad958b54c3cc5e7ce10cca0fb7c463e
mayteguerra1987 <73424773+mayteguerra1987@users.noreply.github.com>
2024-04-19 06:55:28 -0400

    creating cwrn-project project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HomePage from './homepage.component';
+import './homepage.styles.scss'; 
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
